40fae1669730c9b82629880d98b7e94e38c36200
khali70 <k7alifa.a7med70@gmail.com>
2021-01-25 06:34:13 +0200

    doc leaders reducer

--- src/Redux/Reducers/leaders.js
+++ src/Redux/Reducers/leaders.js
@@ -5,10 +5,13 @@ const initState = { isLoading: true, err: null, leaders: [] };
 export const Leaders = (state = initState, { type, payload }) => {
   switch (type) {
     case action.ADD_LEADERS:
+      // update the leaders arr with the payload
       return { ...state, isLoading: false, err: null, leaders: payload };
     case action.LEADERS_LOADING:
+      // return state to default
       return { ...state, ...initState };
     case action.LEADERS_FAILED:
+      // err:payload
       return { ...state, isLoading: false, err: payload, leaders: [] };
     default:
       return state;
