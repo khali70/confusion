e4e1cd4ae5d668757a3a1bbe6d4ba2c55bcabf59
khali70 <k7alifa.a7med70@gmail.com>
2021-01-25 07:07:09 +0200

    doc redux store

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,10 +1,17 @@
+/**
+- createStore => to create the store then add it inside var
+- combineReducers => add meny state to one state
+- applyMiddleware => use thirdparty libs
+ */
 import { createStore, combineReducers, applyMiddleware } from "redux";
+// composeWithDevTools => to activate the redux extention in browser
 import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
+// to disptach actions inside another action
 import thunk from "redux-thunk";
+// redux form handler
 import { createForms } from "react-redux-form";
-
 import { InitialFeedback } from "./Form";
-
+// reducers
 import { Comments } from "./Reducers/comments";
 import { Dishes } from "./Reducers/dishes";
 import { Leaders } from "./Reducers/leaders";
@@ -14,7 +21,9 @@ import { Auth } from "./Reducers/auth";
 import { Favorites } from "./Reducers/favorites";
 
 export const ConfigureStore = () => {
+  // create the store
   const store = createStore(
+    // add all reducers in to one reducer
     combineReducers({
       auth: Auth,
       comments: Comments,
@@ -23,10 +32,14 @@ export const ConfigureStore = () => {
       feedback: FeedBack,
       leaders: Leaders,
       promotions: Promotions,
+      // create the form reducer and store in feedback with init state InitialFeedback
       ...createForms({
         feedback: InitialFeedback,
       }),
     }),
+    // end of combine reucers
+
+    // add middel ware after using DevTooles
     composeWithDevTools(applyMiddleware(thunk))
   );
 
