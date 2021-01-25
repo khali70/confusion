91d52cf2ff3abff05eb7e5e0619ccd5455b538f2
khali70 <k7alifa.a7med70@gmail.com>
2021-01-25 06:24:40 +0200

    doc dishes , comments

--- src/Redux/Reducers/comments.js
+++ src/Redux/Reducers/comments.js
@@ -1,13 +1,17 @@
 import * as action from "../Action/ActionTypes";
 const initState = { err: null, comments: [] };
+
 export const Comments = (state = initState, { type, payload }) => {
   switch (type) {
     case action.ADD_COMMENTS:
+      // update the comment arr with the payload
       return { ...state, isLoading: false, err: null, comments: payload };
     case action.COMMENTS_FAILED:
+      // update error with payload
       return { ...state, isLoading: false, err: payload, comments: [] };
     case action.ADD_COMMENT:
-      console.log(payload);
+      // concat payloads with the comments arr
+      // DOC why use concat
       let comment = payload;
       return { ...state, comments: state.comments.concat(comment) };
     default:
--- src/Redux/Reducers/dishes.js
+++ src/Redux/Reducers/dishes.js
@@ -5,10 +5,13 @@ const initState = { isLoading: true, err: null, dishes: [] };
 export const Dishes = (state = initState, { type, payload }) => {
   switch (type) {
     case action.ADD_DISHES:
+      // update the dishes arr with the payload
       return { ...state, isLoading: false, err: null, dishes: payload };
     case action.DISHES_LOADING:
+      // isloading true
       return { ...state, ...initState };
     case action.DISHES_FAILED:
+      // update error with payload
       return { ...state, isLoading: false, err: payload, dishes: [] };
 
     default:
