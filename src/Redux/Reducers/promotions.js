a1cfb7d4f0e962e317a84204923ba2fa3a5f0752
khali70 <k7alifa.a7med70@gmail.com>
2021-01-25 06:41:19 +0200

    doc promotions reducer

--- src/Redux/Reducers/promotions.js
+++ src/Redux/Reducers/promotions.js
@@ -4,10 +4,13 @@ const initState = { err: null, promotions: [] };
 export const Promotions = (state = initState, { type, payload }) => {
   switch (type) {
     case action.ADD_PROMOS:
+      // update the promotions arr with the payload
       return { ...state, isLoading: false, err: null, promotions: payload };
     case action.PROMOS_FAILED:
+      // err: payload
       return { ...state, isLoading: false, err: payload, promotions: [] };
     case action.PROMOS_LOADING:
+      // reset state
       return { ...state, ...initState };
     default:
       return state;
